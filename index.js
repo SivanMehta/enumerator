30da158225061d4e0aa6017abf7a4fe5decd64ec
Sivan Mehta <Sivanmehta2@gmail.com>
2018-11-03 20:22:10 -0700

    Initial Implementation (#1)

---
+++ index.js
@@ -0,0 +1,47 @@
+class Tree {
+  constructor(value) {
+    this.value = value;
+  }
+
+  get leaf() {
+    return typeof this.nodes === 'undefined';
+  }
+
+  add(arr) {
+    if (this.leaf) {
+      this.nodes = arr.map(e => new Tree(e));
+    } else {
+      this.nodes.forEach(n => n.add(arr));
+    }
+  }
+
+  combine() {
+    if (this.leaf) {
+      return this.value;
+    }
+
+    const leafCombinations = this.nodes.map(n => n.combine());
+    const treeCombinations = leafCombinations.map(l => {
+      return Array.isArray(l) ?
+        l.map(c => { return { ...this.value, ...c }; }) :
+        { ...this.value, ...l };
+    });
+
+    // flatten final array
+    return [].concat.apply([], treeCombinations);
+  }
+}
+
+module.exports = function enumerate (opts) {
+  const tree = new Tree();
+
+  Object.entries(opts).forEach(([key, value]) => {
+    const objects = Array.isArray(value) ?
+      value.map(v => { return { [key]: v }; }) :
+      [{ [key]: value }];
+
+    tree.add(objects);
+  });
+
+  return tree.combine();
+};
